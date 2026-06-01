3173dc298ce90560e26a66fe4d6cc561ed1c89a6
unknown <777docmax777@gmail.com>
2026-06-01 18:52:27 +0200

    ship it

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+let isOpenMenu = false;
 
+const btnElem = document.querySelector('.header-menu-button');
+const listElem = document.querySelector('.header-list');
+const closeIconElem = document.querySelector('.header-menu-icon-close');
+const menuIconElem = document.querySelector('.header-menu-icon');
+btnElem.addEventListener('click', function () {
+  isOpenMenu = !isOpenMenu;
+  if (isOpenMenu) {
+    listElem.style.display = 'block';
+    menuIconElem.style.display = 'none';
+    closeIconElem.style.display = 'block';
+  } else {
+    listElem.style.display = 'none';
+    menuIconElem.style.display = 'block';
+    closeIconElem.style.display = 'none';
+  }
+});
